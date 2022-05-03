996f64e3b5f5166e6d795f29c0b2a61fb3740e25
freddykaberuka <freddykaberuka2009@gmail.com>
2022-05-03 16:14:25 +0200

    destructure codebase

--- index.js
+++ index.js
@@ -1,48 +1,6 @@
-class Book {
-  constructor() {
-    this.bookStore = JSON.parse(localStorage.getItem('singleBook')) || [];
-  }
-
-  getBookList() {
-    const bookList = document.getElementById('book-list');
-    bookList.innerHTML = '';
-    bookList.innerHTML += this.bookStore.map(
-      (el, index) => ` <div class="${index % 2 === 0 ? 'dark' : 'mediumdark'}">
-        <h4>${el.title}</h4>
-        <p>${el.author}</p>
-        <button type="button" id=${el.id} class='remove-button'>Remove</button>
-        </div>`,
-    );
-    const Allbtn = document.querySelectorAll('.remove-button');
-    Allbtn.forEach((btn) => {
-      btn.addEventListener('click', () => {
-        this.deleteBook(btn.id);
-      });
-    });
-  }
-
-  addNewBook(title, author) {
-    const id = this.bookStore.length + 1;
-    const addbook = {
-      title,
-      author,
-      id,
-    };
-    this.bookStore.push(addbook); localStorage.setItem('singleBook', JSON.stringify(this.bookStore));
-    this.getBookList();
-  }
-
-  deleteBook(id) {
-    this.bookStore = this.bookStore.filter((item) => {
-      if (item.id === Number(id)) {
-        return false;
-      }
-      return true;
-    });
-    localStorage.setItem('singleBook', JSON.stringify(this.bookStore));
-    this.getBookList();
-  }
-}
+import { listLink, contactLink, addLink } from './modules/display.js';
+import Book from './modules/book.js';
+import currentDate from './modules/calendar.js';
 
 const booksLibrary = new Book();
 booksLibrary.getBookList();
@@ -54,32 +12,20 @@ formData.addEventListener('submit', (e) => {
   booksLibrary.addNewBook(getTitle, getAuthor);
 });
 
-const listLink = document.getElementById('list-link');
-const addLink = document.getElementById('add-link');
-const contactLink = document.getElementById('contact-link');
-const addDisplay = document.querySelector('.ADD');
-const listDisplay = document.querySelector('.LIST');
-const contactDisplay = document.querySelector('.contact');
+const navLink = document.getElementById('list-link');
+const addNew = document.getElementById('add-link');
+const navContact = document.getElementById('contact-link');
 
-listLink.addEventListener('click', () => {
-  addDisplay.style.display = 'none';
-  contactDisplay.style.display = 'none';
-  listDisplay.style.display = 'flex';
-});
+navLink.addEventListener('click', listLink);
 
-addLink.addEventListener('click', () => {
-  listDisplay.style.display = 'none';
-  contactDisplay.style.display = 'none';
-  addDisplay.style.display = 'flex';
-});
+addNew.addEventListener('click', addLink);
 
-contactLink.addEventListener('click', () => {
-  listDisplay.style.display = 'none';
-  addDisplay.style.display = 'none';
-  contactDisplay.style.display = 'flex';
-});
+navContact.addEventListener('click', contactLink);
 
-window.onload = function () {
+const calendar = document.getElementById('calendar');
+calendar.innerHTML = currentDate;
+
+window.onload = () => {
   document.querySelector('.ADD').style.display = 'none';
   document.querySelector('.contact').style.display = 'none';
 };
---
+++ modules/calendar.js
@@ -0,0 +1,4 @@
+import { DateTime } from './luxon.js';
+const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+
+export default currentDate;
